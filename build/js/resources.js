1c86429f98f4fbf78d1d4c7633d11d6adc6a58df
Sam Franusic <sam@eyefi.com>
2016-01-05 22:52:06 -0800

    Adding MelonJS library and boilerplate files

---
+++ build/js/resources.js
@@ -0,0 +1,27 @@
+game.resources = [
+    {
+        "name": "grass",
+        "type": "image",
+        "src": "data/img/grass.png"
+    },
+    {
+        "name": "metatiles32x32",
+        "type": "image",
+        "src": "data/img/metatiles32x32.png"
+    },
+    {
+        "name": "metatiles35x35",
+        "type": "image",
+        "src": "data/img/metatiles35x35.png"
+    },
+    {
+        "name": "player1",
+        "type": "image",
+        "src": "data/img/sprite/player1.png"
+    },
+    {
+        "name": "grassField",
+        "type": "tmx",
+        "src": "data/map/grassField.tmx"
+    }
+];
---
+++ js/game.js
@@ -0,0 +1,57 @@
+
+/* Game namespace */
+var game = {
+
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
+
+
+    // Run on page load.
+    "onload" : function () {
+        // Initialize the video.
+        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+
+        // add "#debug" to the URL to enable the debug Panel
+        if (me.game.HASH.debug === true) {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
+        }
+
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
+
+        // Load the resources.
+        me.loader.preload(game.resources);
+
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+    // set the "Play/Ingame" Screen Object
+    me.state.set(me.state.PLAY, new game.PlayScreen());
+    me.sys.gravity = 0; 
+    // register our object entities in the object pool
+    me.pool.register("player1", game.PlayerEntity);
+
+
+    // enable the keyboard
+    me.input.bindKey(me.input.KEY.LEFT,  "left");
+    me.input.bindKey(me.input.KEY.RIGHT, "right");
+    me.input.bindKey(me.input.KEY.UP,  "up");
+    me.input.bindKey(me.input.KEY.DOWN, "down");
+     
+    // start the game 
+    me.state.change(me.state.PLAY); }
+};
